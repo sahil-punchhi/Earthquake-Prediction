85491f3ff3cbc5458527aae2872b77ad47a4dfa4
aarushigera55 <aarushigera55@gmail.com>
2019-08-08 14:54:18 +1000

    lta_sta_function 1

--- feature_generation_temp.py
+++ feature_generation_temp.py
@@ -2,7 +2,7 @@
 
 # CURRENTLY DOING
 # - add_trend_feature -> trend_adding_feature
-# - classic_sta_lta   -> classic_lta_sta
+# - classic_sta_lta   -> lta_sta_function
 
  #####################
 # COMPLETED
@@ -25,6 +25,35 @@ class FeatureGenerator(object):
     lr = LinearRegression().fit(index.reshape(-1, 1), array)
     return lr.coef_[0]
 
+
+  def lta_sta_function(self, x, length_sta, length_lta):
+    x_sq = x ** 2
+    sta = np.cumsum(x_sq)
+
+    # Convert to float
+
+    lta = np.require(sta, dtype=np.float)
+
+    # Copy for LTA
+    sta = lta.copy()
+
+    # Compute the STA and the LTA
+    lta[length_lta:] = lta[length_lta:] - lta[:-length_lta]
+    sta[length_sta:] = sta[length_sta:] - sta[:-length_sta]
+    sta /= length_sta
+    lta /= length_lta
+
+    # Pad zeros
+    for i in range(0, length_lta-1):
+        sta[i] = 0
+
+    # Avoid division by zero by setting zero values to tiny float
+    tiny_val = np.finfo(0.0).tiny
+    index_new = lta < tiny_val
+    lta[index_new] = tiny_val
+    return_val = sta / lta
+    return return_val
+
   # ----------- End of Code ----------------
   
   
@@ -32,6 +61,7 @@ class FeatureGenerator(object):
     # add your section here
     pass
 
-array_new = [12,346,5883,25,2,9,635,24,864,2,4664]
+array_new = np.array([12,346,5883,25,2,9,635,24,864,2,4664])
 a = FeatureGenerator()
-print(a.trend_adding_feature(array_new))
+#print(a.trend_adding_feature(array_new))
+print(a.lta_sta_function(array_new,2,3))
