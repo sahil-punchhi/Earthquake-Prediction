577ecf5acafec65087474596482d49a52af5f40b
aarushigera55 <aarushigera55@gmail.com>
2019-08-08 12:20:03 +1000

    trend_adding_feature 1

--- feature_generation_temp.py
+++ feature_generation_temp.py
@@ -7,13 +7,30 @@
 # Completed
 # -
 
-
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 class FeatureGenerator(object):
-  def __init__():
+  def __init__(self):
     pass
+
+  # -----------------Aarushi -----------
+
+  def trend_adding_feature(self, array, absolute=False):
+    arr_len = len(array)
+    index = np.array(range(arr_len))
+    if absolute:
+      array = np.abs(array)
+    lr = LinearRegression().fit(index.reshape(-1, 1), array)
+    return lr.coef_[0]
+
+  # ----------- End of Code ----------------
   
   
   def features(self):
     # add your section here
     pass
+
+array_new = [12,346,5883,25,2,9,635,24,864,2,4664]
+a = FeatureGenerator()
+print(a.trend_adding_feature(array_new))
