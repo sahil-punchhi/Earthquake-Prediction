868975533348e400b46df15c39a41cedf2e4f781
aarushigera55 <45083279+aarushigera55@users.noreply.github.com>
2019-08-08 11:20:37 +1000

    Update feature_generation_temp.py

--- feature_generation_temp.py
+++ feature_generation_temp.py
@@ -1,6 +1,14 @@
 # This is the temp file add your code here
+
+# Currently Doing
 # - add_trend_feature
-  
+
+ #####################
+# Completed
+# -
+
+
+
 class FeatureGenerator(object):
   def __init__():
     pass
