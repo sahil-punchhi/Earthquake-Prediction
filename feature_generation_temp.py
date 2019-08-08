5eac34ceedb2222ec428952402aba386d9cbee2a
amriteshs <48743872+amriteshs@users.noreply.github.com>
2019-08-09 00:03:04 +1000

    implemented feature generation

--- feature_generation_temp.py
+++ feature_generation_temp.py
@@ -1,31 +1,29 @@
 # This is the temp file add your code here
 
 # CURRENTLY DOING
-# classic_sta_lta1_mean - lta_sta_mean_N
-
-# get_features()
-# features() -> generate_features()
-
-#####################
-# COMPLETED
-# moment_   -> moments
-# kstatvar_ -> variable_k_static
-# med -> median
-# kstat_ ->  k_static
-# skew -> skewness
-# mad -> mean_abs_dev
-# kurt -> kurtosis
 # - add_trend_feature -> trend_adding_feature
 # - classic_sta_lta   -> lta_sta_function
 # - calc_change_rate -> change_rate_calculation
 
+# kstat_    -> k_static
+# moment_   -> moments
+# kstatvar_ -> variable_k_static
+
+# get_features()
+
+#####################
+# COMPLETED
+# -
+
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 from scipy import stats
 from collections import defaultdict
+from joblib import Parallel, delayed
+
 
 # ----------------- Aarushi -----------
-
 
 def trend_adding_feature(array, absolute=False):
     arr_len = len(array)
@@ -80,14 +78,14 @@ def change_rate_calculation(x):
 # ----------- End of Code ----------------
 
 
-def generate_features(x):
+def generate_features(x, y, seg_id):
     # collection of features
-    feature_collection={}
+    feature_collection = {}
 
     # collection of intervals
-    feature_intervals={
-        'k_static':list(range(0,5)),
-        'variable_k_stat':[1,2]
+    feature_intervals = {
+        'k_static': list(range(1, 5)),
+        'variable_k_stat': [1, 2]
     }
 
     # add your section here
@@ -95,17 +93,17 @@ def generate_features(x):
     # -----------------Rishabh -----------
 
     for interval in feature_intervals['k_static']:
-        feature_collection['k_static_{interval}']=stats.kstat(x,interval)
+        feature_collection['k_static_{interval}'] = stats.kstat(x, interval)
 
     feature_collection['mean_abs_dev'] = x.mad()
 
     for interval in feature_intervals['variable_k_stat']:
-      feature_collection['variable_k_static_{interval}']=stats.kstatvar(x,interval)
+        feature_collection['variable_k_static_{interval}'] = stats.kstatvar(x, interval)
 
     feature_collection['kurtosis'] = x.kurtosis()
-    
+
     for interval in feature_intervals['k_static']:
-        feature_collection['moments_{interval}']=stats.moment(x,interval)
+        feature_collection['moments_{interval}'] = stats.moment(x, interval)
 
     feature_collection['median'] = x.median()
 
@@ -116,8 +114,80 @@ def generate_features(x):
     return feature_collection
 
 
+# ----------------- Amritesh ---------------
+
+
+def read_data_segments(file, dclass):
+    if dclass == 'train':
+        dtypes = {
+            'acoustic_data': np.int16,
+            'time_to_failure': np.float64
+        }
+
+        df = pd.read_csv(file, dtype=dtypes, iterator=True, chunksize=150000)
+
+        # segments = int(629145480 / 150000)
+
+        for i, j in enumerate(df):
+            x = j.acoustic_data.values
+            y = j.time_to_failure.values[-1]
+            seg_id = 'train_' + str(i)
+            del j
+            yield x, y, seg_id
+    elif dclass == 'test':
+        dtypes = {
+            'acoustic_data': np.int16
+        }
+
+        for i, j in file:
+            dfx = pd.read_csv(j, dtype=dtypes, iterator=True, chunksize=150000)
+            x = j.acoustic_data.values
+            y = -999
+            del dfx
+            yield x, y, i
+
+
+def get_data(file, dclass):
+    feature_list = []
+    res = Parallel(n_jobs=4, backend='threading')(delayed(get_features)(x, y, s) for x, y, s in read_data_segments(file, dclass))
+
+    for r in res:
+        feature_list.append(r)
+
+    return pd.DataFrame(feature_list)
+
+
+def get_features(x, y, seg_id):
+    zc = np.fft.fft(pd.Series(x))
+    realFFT = pd.Series(np.real(zc))
+    imagFFT = pd.Series(np.imag(zc))
+
+    main_dict = generate_features(x, y, seg_id)
+    r_dict = generate_features(realFFT, y, seg_id)
+    i_dict = generate_features(imagFFT, y, seg_id)
+
+    for k, v in r_dict.items():
+        if k not in ['target', 'seg_id']:
+            main_dict[f'fftr_{k}'] = v
+
+    for k, v in i_dict.items():
+        if k not in ['target', 'seg_id']:
+            main_dict[f'ffti_{k}'] = v
+
+    return main_dict
+
+
+# -------------- END OF CODE ---------------
+
 if __name__ == '__main__':
-    array_new = np.array([12,346,5883,25,2,9,635,24,864,2,4664])
-    # print(trend_adding_feature(array_new))
-    # print(lta_sta_function(array_new,2,3))
-    print(change_rate_calculation(array_new))
+    file_path = 'C:\\Users\\amrit\\OneDrive\\Desktop\\files\\'
+
+    train_file = file_path + 'train.csv'
+
+    test_files = []
+    submission = pd.read_csv(file_path + 'sample_submission.csv')
+    for seg_id in submission.seg_id.values:
+        test_files.append((seg_id, file_path + 'test\\' + seg_id + '.csv'))
+
+    get_data(train_file, 'train').to_csv(file_path + 'train_df.csv', index=False)
+    get_data(test_files, 'test').to_csv(file_path + 'test_df.csv', index=False)
