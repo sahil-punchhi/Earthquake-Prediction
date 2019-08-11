cff2803b0110caeab869741c8616c571eaf0784d
Amritesh Singh <amriteshsingh.oshu@gmail.com>
2019-08-11 18:42:02 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
 import warnings
+import matplotlib.pyplot as plt
 from datetime import datetime
 from prediction_cbr import predict as cbrpredict
 from prediction_lgb import predict as lgbpredict
@@ -15,13 +15,21 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 if __name__ == '__main__':
     file_path = os.getcwd() + '/data_files/'
 
+    # function to explore data and generate graphs
+    data_exploration(pd.read_csv(file_path + 'train.csv'))
+
+    # pre-process the data and save results to CSV files
     xtrain, ytrain, xtest, ti = preprocessing(file_path)
-    # predicted_val, out_of_fold = xgbpredict(xtrain, ytrain, xtest)
-    predicted_val, out_of_fold = lgbpredict(xtrain, ytrain, xtest)
+    xtrain.to_csv(file_path + 'result/train_features.csv', index=False)
+    xtest.to_csv(file_path + 'result/test_features.csv', index=False)
+    pd.DataFrame(ytrain).to_csv(file_path + 'result/time_to_failure.csv', index=False)
+
+    # predict function for desired model
+    predicted_val, out_of_fold = xgbpredict(xtrain, ytrain, xtest)
+    # predicted_val, out_of_fold = lgbpredict(xtrain, ytrain, xtest)
     # predicted_val, out_of_fold = cbrpredict(xtrain, ytrain, xtest)
 
-    # data_exploration(pd.read_csv(file_path + 'train.csv', index=False))
-
+    # generate graph to compare actual and predicted values
     plt.figure(figsize=(18, 8))
     plt.plot(ytrain, color='g', label='y_train')
     plt.plot(out_of_fold, color='b', label='y_predicted')
@@ -29,15 +37,11 @@ if __name__ == '__main__':
     plt.title('Prediction')
     plt.show()
 
+    # save time_to_failure values for given segments in sample_submission file
     result = pd.read_csv(file_path + 'sample_submission.csv', index_col='seg_id')
     result['time_to_failure'] = predicted_val
     result.to_csv(file_path + 'result/sample_submission.csv')
 
-    xtrain.to_csv(file_path + 'result/train_features.csv', index=False)
-    xtest.to_csv(file_path + 'result/test_features.csv', index=False)
-    pd.DataFrame(ytrain).to_csv(file_path + 'result/time_to_failure.csv', index=False)
-
     tf = datetime.now()
     tdiff = (tf - ti).total_seconds()
-
     print(f'Time taken for execution: {int(tdiff // 60)}min{int(tdiff % 60)}sec')
